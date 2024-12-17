b68e5e2364cab252de5d01f085518a7e7cd16750
scheltodev <scheltodev@gmail.com>
2024-12-17 13:46:25 +0100

    initial commit

---
+++ restart_rpis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Liste der Raspberry Pi-IPs
+RPI_HOSTS=("192.168.2.112" "192.168.2.113" "1192.168.2.114") # Ersetze durch die tatsächlichen IPs
+
+# Benutzername
+USER="schelto"
+
+# Schleife durch alle Hosts und führe Neustart aus
+for HOST in "${RPI_HOSTS[@]}"; do
+  echo "Starte Raspberry Pi neu: $HOST"
+  ssh "$USER@$HOST" "sudo reboot" && echo "Neustart erfolgreich: $HOST" || echo "Fehler beim Neustart: $HOST"
+done
